f312f9d5699f8abd1c8fe899686e9e44dd42c45e
Heronimus Ronnie <heronimustra@gmail.com>
2017-10-31 17:25:17 +0700

    Iteritems() lib/setup.py python3 compatibility
    Use future.iteritems() for both python2 and python3 compatibility

--- lib/setup.py
+++ lib/setup.py
@@ -10,6 +10,7 @@ from os.path import join as pjoin
 from setuptools import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+from future.utils import iteritems
 import subprocess
 import numpy as np
 
@@ -50,7 +51,7 @@ def locate_cuda():
     cudaconfig = {'home':home, 'nvcc':nvcc,
                   'include': pjoin(home, 'include'),
                   'lib64': pjoin(home, 'lib64')}
-    for k, v in cudaconfig.iteritems():
+    for k, v in iteritems(cudaconfig):
         if not os.path.exists(v):
             raise EnvironmentError('The CUDA %s path could not be located in %s' % (k, v))
 
